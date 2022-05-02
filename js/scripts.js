1b2a17ba6d256e9054844e55bcf9ace30195dff5
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-05-02 16:51:07 +0200

    added first pokemon to pokedex

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+let pokemonList = [{
+    name: 'Pikachu',
+    height: 0.4,
+    category: 'mouse',
+    type: 'electric',
+    weaknesses: 'ground'
+  },
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    category: 'seed',
+    type: ['grass', 'poison]'
+    weaknesses: ['fire', 'ice', 'psychic', 'flying']
+  },
+  {
+    name: 'Mewtwo',
+    height: 2,
+    category: 'genetic',
+    type: 'psychic'
+    weaknesses: ['ghost', 'dark', 'bug']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    category: 'lizard'
+    type: 'fire'
+    weaknesses: ['water', 'ground', 'rock']
+  },
+];
+  
