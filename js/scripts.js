2338a1e8a1f70cc2d6ec6476e7d84690323e1181
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-05-09 12:43:33 +0200

    updated forEach, variable and function inside IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-const pokemonList = [
-  {
+
+const pokemonRepository = (function () {
+  const pokemonList = [
+{
     name: 'Pikachu',
     height: 4,
     types: ['electric'],
@@ -23,14 +25,29 @@ const pokemonList = [
     types: ['fire'],
     category: 'Lizard'
   },
-  
-];
-
-for (let i = 0; i < pokemonList.length; i++)
- if (pokemonList[i].height > 10)
-	document.write('<p>' + "Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + " Wow, that's big!" + '</p>');
-  else {
-    document.write('<p>' + "Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + "." + '</p>');
+  ];
+  function add (pokemon) {
+    pokemonList.push(pokemon);
   }
 
-  
+  function getAll () {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({ name: 'Overkwil', height: 24, types: ['dark', 'poison'], category: 'Pin cluster' });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height > 10) {
+    document.write('<p class="special">' + pokemon.name, '(height: ' + pokemon.height + ')-Wow!-that\'s big</p>');
+  } else {
+    document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + ')</p>');
+  }
+});
